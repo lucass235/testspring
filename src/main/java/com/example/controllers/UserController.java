2ad3bc1eb21208f6b6cc6aa09290ead58ea27c48
lucas <lucass235@gmail.com>
2024-10-10 10:53:45 -0300

    first commit

---
+++ src/main/java/com/example/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.example.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entities.User;
+import com.example.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> findAll() {
+        List<User> result = repository.findAll();
+        System.out.println("Users found: " + result.size());
+        return result;
+    }
+
+}
